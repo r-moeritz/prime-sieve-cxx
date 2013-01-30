bd8e91cf20add152c20dae426256716b55ab0258
Ralph Moritz <ralmoritz@gmail.com>
2013-01-30 10:44:23 +0200

    New function: primes_lt_n. Finds primes < n very quickly.

--- include/prime_sieve/sieve.hh
+++ include/prime_sieve/sieve.hh
@@ -7,6 +7,9 @@
 namespace prime_sieve {
   template <typename T>
   std::vector<T> primes_in_range(const std::pair<T, T>&);
+
+  template <typename T>
+  std::vector<T> primes_lt_n(const T n);
 }
 
 #include "sieve.inl-cc"
--- src/sample/sample.cc
+++ src/sample/sample.cc
@@ -1,17 +1,26 @@
 #include "prime_sieve/sieve.hh"
 #include <iostream>
+#include <array>
 #include <ctime>
 
 using namespace std;
 using namespace prime_sieve;
 
 int main() {
-  for (;;) {
+  array<int, 4> nv = { 1000000, 10000000, 100000000, 1000000000 };
+
+  for (auto& n : nv) {
     auto c1 = clock();
-    auto primes = primes_in_range( make_pair(1, 10000000) );
+	auto primes = primes_lt_n(n);
     auto c2 = clock();
-    getchar();
-    cout << "Generated " << primes.size() << " primes in "
+    cout << "Found " << primes.size() << " primes in "
          << (c2 - c1)/static_cast<double>(CLOCKS_PER_SEC) << " seconds" << endl;
   }
 }
+
+/*
+Found 78499 primes in 0.019 seconds
+Found 664580 primes in 0.129 seconds
+Found 5761456 primes in 2.119 seconds
+Found 53327625 primes in 33.126 seconds
+*/
